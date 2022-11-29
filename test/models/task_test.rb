f1afa3c88fe6935e494a8031c48f25dc58ba902e
Kate Pelinggon <katepelinggon@gmail.com>
2022-11-29 19:48:42 +0800

    add test to task test

--- test/models/task_test.rb
+++ test/models/task_test.rb
@@ -8,9 +8,17 @@ class TaskTest < ActiveSupport::TestCase
         assert_not task.save, 'Saved the Task without a name'
     end
 
+    test "name should not have less than 3 characters" do
+        task = Task.new
+        task.name = 'ts'
+        assert_not task.save, "Saved the Task with name less than 3 characters"
+    end
+
     test "should not save Task without description" do
         task = Task.new
         task.name = 'Task 1'
         assert_not task.save, "Saved the Task without a description"
     end
+
+
 end
